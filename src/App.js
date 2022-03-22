c11c3612b8e3372c25bad8d731236fcf8f48575e
Bartosz Kwasniewski <support@k1pro.net>
2022-03-22 12:51:23 +0100

    First Publish for Expense Tracker App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h2>Barts App</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
